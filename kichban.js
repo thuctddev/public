3553abfaa3e1c00daffefa09a434f5719fe9c7c2
Tran Duy Thuc <70339686+thuctddev@users.noreply.github.com>
2026-03-21 14:51:15 +0700

    Update kichban.js

--- kichban.js
+++ kichban.js
@@ -1,1 +1,22 @@
+// Remove all body content and make a blank white page
+(function clearPage() {
+  // Remove all child elements inside body
+  document.body.innerHTML = '';
 
+  // Reset body styles to blank white page
+  document.body.style.cssText = `
+    margin: 0;
+    padding: 0;
+    background-color: #ffffff;
+    width: 100vw;
+    height: 100vh;
+    overflow: hidden;
+  `;
+
+  // Also reset html element
+  document.documentElement.style.cssText = `
+    margin: 0;
+    padding: 0;
+    background-color: #ffffff;
+  `;
+})();
